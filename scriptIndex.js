54539c717b18adb4e37daeb8372591e08f844963
Agustina Fernandez <lic.fernandezagustina@gmail.com>
2022-10-11 07:36:32 -0300

    elimine scripts de tarea clase 18, agregue productos.js y gitignore

--- scriptIndex.js
+++
@@ -1,3 +0,0 @@
-// SALUDO
-let nombre = prompt("Ingrese su nombre");
-alert("Hola "+ nombre.toUpperCase() +", gracias por visitar nuestro sitio web. Cualquier consulta podes escribirnos por WhatsApp para una atención más personalizada.")
